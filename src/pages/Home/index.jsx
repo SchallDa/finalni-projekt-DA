2533ccc9b5ae244b038d5fe6ebb6a1d5089d3f42
Daniela Schalldach <sulikova.daniela@gmail.com>
2023-06-08 22:18:28 +0200

    added Home into pages

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.css';
+
+export const Home =()=> {
+    return (
+        <>
+        <div className="container"> 
+        homepage
+        </div>
+        </>
+    )
+};
