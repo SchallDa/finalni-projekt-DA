28dd85a61186d74f92211b07e0100e17c4cd91ef
Kristýna Adlofová <kristyna.adlofova@gmail.com>
2023-06-15 00:41:53 +0200

    reduce size of the images + correction of HP picture

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -6,18 +6,18 @@ export const HomePage = () => {
   return (
     <>
       <div className="container container--main">
-        <div className="main__image"></div>
+        <img src="./img/wool.jpg" className="main__image"></img>
         <p className="main__intro">
           Chceš omezit množství oblečení ve své skříni, ale nevíš jak na to?
           Pomůžeme ti.
         </p>
 
         <div className="buttons">
-          <Link className="button button--big" to="/vyhledat-odev">
+          <Link className="button" to="/vyhledat-odev">
             Vyhledat oděv
           </Link>
 
-          <Link className="button button--big" to="/o-projektu">
+          <Link className="button" to="/o-projektu">
             O projektu
           </Link>
         </div>
