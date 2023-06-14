0999b7de4df87610ef79e54d17528be2aef68fbe
Kristýna Adlofová <kristyna.adlofova@gmail.com>
2023-06-15 01:59:18 +0200

    Fabric Result page style

--- src/pages/FabricResults/index.jsx
+++ src/pages/FabricResults/index.jsx
@@ -1,30 +1,59 @@
-import React from "react";
+import React from 'react';
 import './style.css';
-import { useParams } from "react-router-dom";
-import { Clothes } from "../../consts";
+import { useParams } from 'react-router-dom';
+import { Clothes } from '../../consts';
+import { Link } from 'react-router-dom';
 
-export const FabricResults=()=>{
-    const{uniqueID}=useParams();
-    const {header, icon, fiberLink, pros, cons, maintanance, eco}=Clothes[uniqueID]
-    
+export const FabricResults = () => {
+  const { uniqueID } = useParams();
+  const { header, icon, fiberLink, pros, cons, maintanance, eco } =
+    Clothes[uniqueID];
 
-    
-    return (
-<>
-<img src={`/img/${icon}.svg`} alt="clothes_icon" />
-<span> <img src={`/img/${fiberLink}.jpg`} alt="fiber_link_to_materials" /></span>
-<h2>{header}</h2>
-<h3>Proč zrovna tento materiál</h3>
-<p>{pros}</p>
-<h3>Nevýhody</h3>
-<p>{cons}</p>
-<h3>Údržba</h3>
-{/* spodní řádek připraven pro pole ikon */}
-<div>  {maintanance.map((mIcons)=> <img key={mIcons} src={`/img/${mIcons}.svg`} alt="care_icons" />)} 
-</div> 
-<h3>Udržitelnost</h3>
-<p>{eco}</p>
-{/* Tyno, ty divy, p a celkově styly, jsou tam jen provizorne, abych ty propsy do něčeho obalila. Měň to a upravuje dle libosti */}
-</>
-    )
-}
+  return (
+    <div className="form_container">
+      <div className="form__header">
+        <img
+          src={`/img/${icon}.svg`}
+          alt="clothes_icon"
+          className="form__image form__image--pink"
+        />
+
+        <h2 class="form__headline">{header}</h2>
+      </div>
+
+      {/* Potřeba napojit linky na stránku s konkrétním materiálem */}
+      <Link to="#">
+        <img
+          src={`/img/${fiberLink}.jpg`}
+          className="result__image"
+          alt="fiber_link_to_materials"
+        />
+      </Link>
+
+      <h3 className="result__question">Proč zrovna tento materiál</h3>
+      <p className="result__text">{pros}</p>
+      <h3 className="result__question">Nevýhody</h3>
+      <p className="result__text">{cons}</p>
+      <h3 className="result__question">Údržba</h3>
+      {/* spodní řádek připraven pro pole ikon */}
+      <div className="result__text">
+        {' '}
+        {maintanance.map((mIcons) => (
+          <img
+            key={mIcons}
+            src={`/img/${mIcons}.svg`}
+            alt="care_icons"
+            className="form__image"
+          />
+        ))}
+      </div>
+      <h3 className="result__question">Udržitelnost</h3>
+      <p>{eco}</p>
+      {/* Tyno, ty divy, p a celkově styly, jsou tam jen provizorne, abych ty propsy do něčeho obalila. Měň to a upravuje dle libosti */}
+
+      <Link className="button button-form" to="/vyhledat-odev">
+        Zpět
+      </Link>
+    </div>
+  );
+};
