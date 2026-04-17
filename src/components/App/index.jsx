88da0e9aefb3090c21f652a5444c71c55184a19a
Daniela Schalldach <sulikova.daniela@gmail.com>
2026-04-17 22:52:00 +0200

    adding analyitcs in npm

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,9 +1,16 @@
-import React from "react";
-import { Outlet} from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet } from "react-router-dom";
 import { Header } from "../Header";
 import { Footer } from "../Footer";
+import ReactGA from "react-ga4";
+import { use } from "react";
+
+const GA_ID = "G-XK10LZ90XX";
 
 export const App = () => {
+  useEffect(() => {
+    ReactGA.initialize(GA_ID);
+  }, []);
   return (
     <>
       <Header />
